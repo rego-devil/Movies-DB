4957a7a1297be71d373f9b51c237be21d2356487
Egor Burkovski <Egor_Burkovski@epam.com>
2018-04-11 13:38:35 +0300

    default setting + create Hello world using other ways

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack');
+
+const path = require("path");
+
+const APP_DIR = path.resolve(__dirname, "src");
+const BUILD_DIR = path.resolve(__dirname, "public");
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+const config = {
+  mode: "development",
+  watch: true,
+  
+  entry: {
+    javascript: APP_DIR + "/index"
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: "[name].bundle.js"
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: ['babel-loader'],
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // favicon: `${APP_DIR}/favicon.ico`,
+      template: `${APP_DIR}/index.html`,
+      inject: false
+    })
+  ]
+}
+
+module.exports = config;
