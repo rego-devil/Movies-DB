e27468b9dc8c73f3e3c3b52a7537347b2a01985a
Egor Burkovski <Egor_Burkovski@epam.com>
2018-05-22 16:38:30 +0300

    unit testing

---
+++ src/client/__tests__/film-details.spec.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {shallow, mount, render} from 'enzyme';
+import {FilmDetails} from '../components/film-details';
+
+describe('film Item component', () => {
+    let renderedComponent;
+    const minProps = {
+        title: 'default',
+        genres: [],
+        release_date: '1939-08-15',
+        vote_average: 8,
+        poster_path: 'ulrToImg',
+        runtime: ''
+    }
+
+    test('+++ Match snapshot', () => {
+        const renderedComponent = shallow(<FilmDetails {...minProps} />)
+        expect(renderedComponent).toMatchSnapshot();
+    });
+
+    test('renders without crashing', () => {
+        const renderedComponent = shallow(<FilmDetails {...minProps} />)
+        expect(renderedComponent.length).toEqual(1);
+
+        // console.log(renderedComponent.debug());
+    });
+
+    test('Check runtime condition', () => {
+        let renderedComponent, runtime;
+        
+        runtime = 128;
+        renderedComponent = shallow(<FilmDetails {...minProps} runtime={runtime}  />)
+        expect(renderedComponent.find('.filmDetails__duration').text()).toEqual(`${runtime} min.`);
+
+        runtime = '';
+        renderedComponent = shallow(<FilmDetails {...minProps} runtime={runtime}  />)
+        expect(renderedComponent.find('.filmDetails__duration').text()).toEqual(runtime);
+
+        // console.log(renderedComponent.debug());
+    });
+
+    // test('click event', () => {
+    //     const wrapper = shallow(<FilmItem  />);
+    //     // wrapper.find('.filmItem').simulate('click');
+    // });
+});
