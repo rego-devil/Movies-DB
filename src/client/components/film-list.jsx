d23062ee5be9c7e679fd7f6785e58068a9ef8bff
Egor Burkovski <Egor_Burkovski@epam.com>
2018-06-07 17:03:15 +0300

    refactoring sorting

--- src/client/components/film-list.jsx
+++ src/client/components/film-list.jsx
@@ -3,29 +3,53 @@ import {FilmItem} from './';
 import { Loading } from './loading';
 import { Link } from 'react-router-dom';
 
-export const FilmList = ({films, isFetching, error}) => (
-	<React.Fragment>
-		{
-			isFetching ? <Loading /> : ''
-		}
-		{
-			films.length ? (
-			<ul className="filmList">
-				{
-					films.map((item) => 
-						<li key={item.id} className="filmItem">
-							<Link to={`/film/${item.id}`} ><FilmItem {...item} /></Link>
-						</li>
-					)
-				}
-			</ul>
-		) : (
+export class FilmList extends React.Component { 
+
+	componentWillReceiveProps() {
+		
+		const {sortOrder, sortBy, films} = this.props;
+		films.sort((a,b) => {
+			if( sortOrder === 'desc') {
+				if (a[sortBy] > b[sortBy]) return -1;
+				if (a[sortBy] < b[sortBy]) return 1;
+			}
+			if( sortOrder === 'asc') {
+				if (a[sortBy] > b[sortBy]) return 1;
+				if (a[sortBy] < b[sortBy]) return -1;
+			}
+			return 0;
+		});
+
+	}
+
+	render() {
+		const {films, isFetching, error, sortOrder, sortBy} = this.props;
+	
+		return (
 			<React.Fragment>
-				<div className="error">{error}</div>
-				<div className="noFilms">No films found</div>
+				{
+					isFetching ? <Loading /> : ''
+				}
+				{
+					films.length ? (
+					<ul className="filmList">
+						{
+							films.map((item) => 
+								<li key={item.id} className="filmItem">
+									<Link to={`/film/${item.id}`} ><FilmItem {...item} /></Link>
+								</li>
+							)
+						}
+					</ul>
+				) : (
+					<React.Fragment>
+						<div className="error">{error}</div>
+						<div className="noFilms">No films found</div>
+					</React.Fragment>
+				)}
 			</React.Fragment>
-		)}
-	</React.Fragment>
-)
+		)
+	}
+}
 
 
